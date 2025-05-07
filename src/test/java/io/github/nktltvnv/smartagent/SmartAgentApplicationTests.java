3931638998610b5c5c026f316a0c56111bb96696
Mykyta Lytvynov <nktltvnv.v@gmail.com>
2025-05-07 11:57:13 +0300

    initial commit

---
+++ src/test/java/io/github/nktltvnv/smartagent/SmartAgentApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.nktltvnv.smartagent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartAgentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
