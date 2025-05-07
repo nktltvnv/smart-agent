bc18ed15e688025b45b39267f8cc36a0f8866125
Mykyta Lytvynov <nktltvnv.v@gmail.com>
2025-05-08 00:45:22 +0300

    added a resource server default configuration

---
+++ src/main/java/io/github/nktltvnv/smartagent/config/SecurityConfiguration.java
@@ -0,0 +1,26 @@
+package io.github.nktltvnv.smartagent.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain webFilterChain(final ServerHttpSecurity http) {
+        return http.authorizeExchange(exchanges -> exchanges
+                        .pathMatchers("/chat/stream/personalized")
+                        .authenticated()
+                        .anyExchange()
+                        .permitAll())
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()))
+                .cors(ServerHttpSecurity.CorsSpec::disable)
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+}
